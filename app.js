1c5c8d41f9c29fa53ace0fa8f9118c9f9fee4368
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-07-22 19:46:16 -0500

    subida de archivos

---
+++ app.js
@@ -0,0 +1,25 @@
+
+// Esto es un comentario en JavaScript
+console.log("hola, sp");
+
+document.getElementById('miBoton').addEventListener('click',function(){
+    alert('has hecho click');
+})
+
+document.getElementById('hora').addEventListener('click', function(){
+    document.getElementById('fecha').innerHTML = new Date();
+});
+
+document.getElementById('mensaje').innerHTML = "holaaaaaa"
+
+document.getElementById('encender').addEventListener('click', function(){
+    document.getElementById('imagen').src='prender.gif'
+})
+
+document.getElementById('apagar').addEventListener('click', function(){
+    document.getElementById('imagen').src='apagar.gif'
+})
+
+document.getElementById('agrandar').addEventListener('click', function(){
+    document.getElementById('texto').style.fontSize='35px'
+})
---
+++ myScript.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+
+function Sumas(){
+    document.getElementById("suma").innerHTML = 5 + 5 
+}
+
+
+function basico(){
+    let x;
+    x = 6
+    document.getElementById("prueba").innerHTML = x
+}
